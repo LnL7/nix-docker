0bc28d9ce073cf9ba6fea0459e80ffa681008e05
Daiderd Jordan <daiderd@gmail.com>
2019-03-02 09:27:18 +0100

    fix hash

--- srcs/2019-03-01.nix
+++ srcs/2019-03-01.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = https://releases.nixos.org/nixpkgs/nixpkgs-19.09pre170896.6e5caa3f8ac/nixexprs.tar.xz;
-    sha256 = "10lqmzry265cvmvy330f2k41gk1k88yfmvm3ljknwkd4p8hn2gg:";
+    sha256 = "10lqmzry265cvmvy330f2k41gk1k88yfmvm3ljknwkd4p8hn2ggi";
   };
 
   dontBuild = true;
